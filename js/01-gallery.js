e08a617dce2f5fff4f396d6fb140d822bcafe986
CherniukAnna <chernukanna@gmail.com>
2023-01-24 19:02:18 +0200

    галерея 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.innerHTML = galleryItems
+  .map(
+    (element) => `<div class="gallery__item">
+  <a class="gallery__link" href="${element.original}">
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+galleryEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  let instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt="${event.target.alt}">`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", close);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", close);
+      },
+    }
+  );
+  instance.show();
+  function close(e) {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+  }
+});
