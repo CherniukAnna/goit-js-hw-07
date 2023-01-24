ed7f11d9ddee5dfea9abdaeeac02a16b451e7155
CherniukAnna <chernukanna@gmail.com>
2023-01-24 19:02:33 +0200

    галерея 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.innerHTML = galleryItems
+  .map(
+    (elem) => `<a class="gallery__item" href="${elem.original}">
+  <img class="gallery__image" src="${elem.preview}" alt="" title="${elem.description}" />
+</a>`
+  )
+  .join("");
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
